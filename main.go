001c7424bdba6e9a4035f2ea609d260c651536d0
Izaak Dale <izaakdaledev@gmail.com>
2024-04-07 23:06:50 -0700

    add host port env vars

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
-	http.ListenAndServe(":80", mux)
+	http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")), mux)
 }
